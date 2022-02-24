7f9525d3794141fdb6b588e0719e6e50304284b4
zaydzuhri <zaydzuhri@gmail.com>
2022-02-24 23:54:50 +0700

    stdlib and printString

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -7,21 +7,10 @@
 #include "header/kernel.h"
 
 int main() {
-    // char *string = "This is MayanOS "; // Deklarasikan variabel pada awal scope
-    // int i = 0;
-    // int strLen;
-    // for (strLen = 0; string[strLen] != '\0'; ++strLen) // Hitung panjang string
-    //     ;
-    // for (i = 0; i < strLen; i++) {
-    //     byte warna = 0xD;
-    //     putInMemory(0xB000, 0x8000 + 2 * i, string[i]);
-    //     putInMemory(0xB000, 0x8001 + 2 * i, warna);
-    // }
-
-    interrupt(0x10, 0x0E54, 0x0003, 0x0, 0x0);
-    interrupt(0x10, 0x0E45, 0x0003, 0x0, 0x0);
-    interrupt(0x10, 0x0E53, 0x0003, 0x0, 0x0);
-    interrupt(0x10, 0x0E54, 0x0003, 0x0, 0x0);
+    char *string = "Hello, World! This is MayanOS";
+    // Set Video Mode
+    interrupt(0x10, 0x0012, 0, 0, 0);
+    printString(string);
 
     while (true)
         ;
@@ -29,4 +18,12 @@ int main() {
 
 void handleInterrupt21(int AX, int BX, int CX, int DX) {
     // Definisi kosong
+}
+
+void printString(char *string) {
+    int i = 0;
+    for (i = 0; i < strlen(string); i++) {
+        int AX = 0x0E00 + string[i];
+        interrupt(0x10, AX, 0x0006, 0x0, 0x0);
+    }
 }
--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,49 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+int div(int a, int b) {
+    return a / b;
+}
+
+// int mod(int a, int n) {
+//     return a % n;
+// }
+
+void memcpy(byte *dest, byte *src, unsigned int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        dest[i] = src[i];
+    }
+}
+
+// Mengembalikan panjang suatu null terminated string
+unsigned int strlen(char *string) {
+    int i;
+    for (i = 0; string[i] != '\0'; i++)
+        ;
+    return i;
+}
+
+bool strcmp(char *s1, char *s2) {
+    int i;
+    for (i = 0; i < strlen(s1); i++) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void strcpy(char *dst, char *src) {
+    int i;
+    for (i = 0; i < strlen(src); i++) {
+        dst[i] = src[i];
+    }
+}
+
+void clear(byte *ptr, unsigned int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        ptr[i] = 0;
+    }
+}
