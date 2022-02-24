7dcdf1777c911361feba68a2973682342c345e54
Dzakfr1006 <16520278@std.stei.itb.ac.id>
2022-02-25 00:25:26 +0700

    Add clearScreen() to kernel.c

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -9,8 +9,10 @@
 int main() {
     char *string = "Hello, World! This is MayanOS";
     // Set Video Mode
-    interrupt(0x10, 0x0012, 0, 0, 0);
+    interrupt(0x10, 0x03, 0, 0, 0);
     printString(string);
+    interrupt(0x10, 0x0200, 0x00, 0x0, 0x0002);
+    clearScreen();
 
     while (true)
         ;
@@ -26,4 +28,10 @@ void printString(char *string) {
         int AX = 0x0E00 + string[i];
         interrupt(0x10, AX, 0x0006, 0x0, 0x0);
     }
+}
+
+void clearScreen() {
+    interrupt(0x10, 0x00, 0, 0, 0);
+    interrupt(0x10, 0x03, 0, 0, 0);
+    interrupt(0x10, 0x0200, 0x00, 0x0, 0x00);
 }
